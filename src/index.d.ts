910bb023af28102186e0d30b0e145479010cbd74
James <james@arnott.tech>
2023-10-01 22:33:00 +0100

    feat: added tailwind + daisyui styled popup

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,5 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    PLASMO_PUBLIC_GTAG_ID?: string
-  }
-}
-
 interface Window {
-  dataLayer: Array
-  gtag: (a: string, b: any, c?: any) => void
+  exampleModal: any;
 }
 
 declare module '*.svg' {
