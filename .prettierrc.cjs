7c05f3652cd183cef6848e44b933a2cff9bca5c9
James <james@arnott.tech>
2024-04-30 21:05:18 +0100

    chore: prettier/eslint config + updated dependencies

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,17 +1,30 @@
 /**
  * @type {import('prettier').Options}
  */
-module.exports = {
+const config = {
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   semi: true,
-  singleQuote: true,
+  singleQuote: false,
   trailingComma: "none",
   bracketSpacing: true,
   bracketSameLine: true,
-  plugins: [require.resolve("@plasmohq/prettier-plugin-sort-imports")],
-  importOrder: ["^@plasmohq/(.*)$", "^~(.*)$", "^[./]"],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-}
+  endOfLine: false,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<BUILTIN_MODULES>", // Node.js built-in modules
+    "<THIRD_PARTY_MODULES>", // Imports not matched by other special words or groups.
+    "", // Empty line
+    "^@plasmo/(.*)$",
+    "",
+    "^@plasmohq/(.*)$",
+    "",
+    "^~(.*)$",
+    "",
+    "^[./]"
+  ]
+};
+
+module.exports = config;
+
